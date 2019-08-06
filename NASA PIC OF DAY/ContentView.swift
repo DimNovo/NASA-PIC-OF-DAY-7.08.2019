136f66cfe4338b2dc6a0dc192c166baff9fd6e72
Dmitry Novosyolov <i1710@icloud.com>
2019-08-06 20:57:47 +0300

    Initial Commit

---
+++ NASA PIC OF DAY/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NASA PIC OF DAY
+//
+//  Created by Dmitry Novosyolov on 06/08/2019.
+//  Copyright © 2019 Dmitry Novosyolov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
